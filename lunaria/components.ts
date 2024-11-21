b686d814e6b1957298244d0ef7643b079db15222
GuoJiKun <guojikun486546@163.com>
2024-11-21 10:39:00 +0800

    Improving CSP Security

--- lunaria/components.ts
+++ lunaria/components.ts
@@ -3,9 +3,9 @@ import { html } from '@lunariajs/core';
 export const TitleParagraph = () =>
   html`<p>
       If you're interested in helping us translate
-      <a href="https://tauri.app/">tauri.app</a> into one of the languages listed below,
-      you've come to the right place! This auto-updating page always lists all the content that
-      could use your help right now.
+      <a href="https://tauri.app">tauri.app</a> into one of the languages listed below, you've come
+      to the right place! This auto-updating page always lists all the content that could use your
+      help right now.
     </p>
 
     <p>
